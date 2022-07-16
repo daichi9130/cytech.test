5fc5e81a5c354193d64e1192d94d02fadf772b26
daichi <nakanodaichi@daichi.local>
2022-07-16 23:28:32 +0900

    relation完了

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function sales()
+    {
+        return $this->hasMany('App\Models\Sale');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo('App\Models\Company');
+    }
 }
